1f3b59ea6ea418a1b3285d6f63676cb27f6bb53e
sychonix <110755590+sychonix@users.noreply.github.com>
2024-02-16 22:13:05 +0700

    Update index.tsx

--- modules/Home/AboutUs/index.tsx
+++ modules/Home/AboutUs/index.tsx
@@ -97,7 +97,7 @@ export function AboutUs() {
               Your Trusted Validator & Interchain Utility
             </Title>
             <Text c="dimmed">
-              Raven Node is a trusted validator & interchain utility provider with few simple steps
+              Sychonix Node is a trusted validator & interchain utility provider with few simple steps
               you can delegate funds to our trusted validators or explore our services where you can
               find tools that will be useful for node operators and developers.
             </Text>
